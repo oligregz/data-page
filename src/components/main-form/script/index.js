dbcddbb1337d89b118376f5708425aaf4e3295b6
oligregz <gregorysak90@gmail.com>
2023-10-14 10:22:49 -0300

    feat: add data form capture

---
+++ src/components/main-form/script/index.js
@@ -0,0 +1,29 @@
+import renderObj from "../../../utils/dataJson.js";
+
+const formData = {};
+
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.querySelector('form.form-horizontal');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    localStorage.clear();
+
+    const inputs = form.querySelectorAll('input');
+    inputs.forEach(input => {
+      formData[input.id] = input.value;
+    });
+    
+    localStorage.setItem('formData', JSON.stringify(formData));
+    
+    alert('Dados capturados e armazenados no localStorage.');
+    
+    
+    form.reset();
+    renderObj()
+    formData = {}
+  });
+});
+
+export default formData;
---
+++ src/utils/dataJson.js
@@ -0,0 +1,7 @@
+import formData from "../components/main-form/script/index.js";
+
+const renderObj = () => {
+  console.log(formData);
+}
+
+export default renderObj;
