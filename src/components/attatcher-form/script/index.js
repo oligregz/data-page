ff4ed8d072b88c66dbf2b5e37b18464c93e1b0d4
oligregz <gregorysak90@gmail.com>
2023-10-18 13:22:27 -0300

    feat: add download file

--- src/components/attatcher-form/script/index.js
+++ src/components/attatcher-form/script/index.js
@@ -1,31 +1,31 @@
-const filesList = document.querySelector('.files-list');
+const filesList = document.querySelector(".files-list");
 
 const getNewLi = (fileName) => {
-  const fileLi = document.querySelector('.file');
+  const fileLi = document.querySelector(".file");
 
   if (fileLi) {
     const clonedLi = fileLi.cloneNode(true);
-    const content = clonedLi.querySelector('.content');
-    const docFile = content.querySelector('.doc-file');
-    docFile.textContent = fileName || 'Adicionar Documento';
+    const content = clonedLi.querySelector(".content");
+    const docFile = content.querySelector(".doc-file");
+    docFile.textContent = fileName || "Adicionar Documento";
     filesList.appendChild(clonedLi);
     attachFileListener(clonedLi);
     attachDeleteListener(clonedLi);
   }
-}
+};
 
 const attachFileListener = (li) => {
-  const contentElement = li.querySelector('.content');
-  contentElement.addEventListener('click', () => {
-    const input = document.createElement('input');
-    input.type = 'file';
-    input.accept = 'application/pdf,.doc,.docx,.txt';
+  const contentElement = li.querySelector(".content");
+  contentElement.addEventListener("click", () => {
+    const input = document.createElement("input");
+    input.type = "file";
+    input.accept = "application/pdf,.doc,.docx,.txt";
 
-    input.addEventListener('change', (event) => {
+    input.addEventListener("change", (event) => {
       const file = event.target.files[0];
       if (file) {
         const fileName = file.name;
-        const docFile = contentElement.querySelector('.doc-file');
+        const docFile = contentElement.querySelector(".doc-file");
         docFile.textContent = fileName;
       }
     });
@@ -35,19 +35,36 @@ const attachFileListener = (li) => {
 };
 
 const attachDeleteListener = (li) => {
-  const deleteIcon = li.querySelector('.lixeira-icon');
-  deleteIcon.addEventListener('click', (event) => {
+  const deleteIcon = li.querySelector(".lixeira-icon");
+  deleteIcon.addEventListener("click", (event) => {
     event.stopPropagation();
     li.remove();
   });
 };
 
-const firstLi = document.querySelector('.file');
+const attachDownloadListener = (li, fileName) => {
+  const downloadIcon = li.querySelector(".visualizar-icon");
+  downloadIcon.addEventListener("click", (event) => {
+    event.stopPropagation();
+    let data = "Um conteúdo qualquer";
+    let blob = new Blob([data], { type: "text/plain;charset=utf-8;" });
+    const link = window.document.createElement("a");
+    link.href = window.URL.createObjectURL(blob);
+    link.download = "export.txt";
+    link.click();
+    window.URL.revokeObjectURL(link.href);
+  });
+};
+
+const firstLi = document.querySelector(".file");
 if (firstLi) {
   attachFileListener(firstLi);
   attachDeleteListener(firstLi);
+  attachDownloadListener(firstLi);
 }
 
-document.querySelector('.form-group button.product').addEventListener('click', () => {
-  getNewLi();
-});
+document
+  .querySelector(".form-group button.product")
+  .addEventListener("click", () => {
+    getNewLi();
+  });
